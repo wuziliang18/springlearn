54350cda9bd310ec17fc365732fe23ad3e46f49a
ziliang.wu <ziliang.wu@wuage.com>
2019-04-26 09:50:32 +0800

    'aspparam'

--- spirngboot/src/main/java/com/wuzl/learn/spring/boot/config/AopConfig.java
+++ spirngboot/src/main/java/com/wuzl/learn/spring/boot/config/AopConfig.java
@@ -4,22 +4,37 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
 @Component
 @Aspect
 public class AopConfig {
-	//使用切点可以复用
+	// 使用切点可以复用
 	@Pointcut("execution(* com.wuzl.learn.spring.boot.service.*.*(..))")
 	public void pointcut() {
-		
+
 	}
-	//所有bean都拦截
-//	@Before("execution(* com.wuzl.learn.spring.boot.service.*.*(..))")
-	//只拦截personservice的和不拦截persionservice的  不生效? 需要去掉引号
-//	@Before("execution(* com.wuzl.learn.spring.boot.service.*.*(..)) and bean(personService)")
-//	@Before("execution(* com.wuzl.learn.spring.boot.service.*.*(..)) and !bean(personService)")
-	//使用切点
+	
+	@Pointcut("execution(* com.wuzl.learn.spring.boot.service.*.*(String)) && args(stringParam)")
+	public void pointcutParam(String stringParam) {
+
+	}
+
+	// 所有bean都拦截
+	// @Before("execution(* com.wuzl.learn.spring.boot.service.*.*(..))")
+	// 只拦截personservice的和不拦截persionservice的 不生效? 需要去掉引号
+	// @Before("execution(* com.wuzl.learn.spring.boot.service.*.*(..)) and
+	// bean(personService)")
+	// @Before("execution(* com.wuzl.learn.spring.boot.service.*.*(..)) and
+	// !bean(personService)")
+	// 使用切点
+	@Before("pointcutParam(stringParam)")
+	public void before(String stringParam) {
+		System.out.println("before:>>param:" + stringParam);
+	}
+
 	@Before("pointcut()")
 	public void before() {
 		System.out.println("before");
 	}
+
 }
